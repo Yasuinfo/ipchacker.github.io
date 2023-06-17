e4d11321424186d14ebc941510dd31eef25d2e65
Yasu info <39099655+Yasuinfo@users.noreply.github.com>
2023-06-17 15:44:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+async function getCloudflareJSON(){
+let data = await fetch('https://1.1.1.1/cdn-cgi/trace').then(res=>res.text())
+let arr = data.trim().split('\n').map(e=>e.split('='))
+return Object.fromEntries(arr)
+}
+async function somefunc(){
+let cloudflareJSON = await getCloudflareJSON()
+document.querySelector('#header').innerHTML = cloudflareJSON.ip
+console.log(cloudflareJSON.ip)
+}
 
+somefunc().then(console.log)
